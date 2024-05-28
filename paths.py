f62f9796da7334ff397a63284fe0c9a65ccfdd72
Mo <mohamedabdelhamid3030@gmail.com>
2024-05-28 21:11:04 +0300

    uploaded notebooks

---
+++ paths.py
@@ -0,0 +1,3 @@
+dataset_cfg_path = "./config/time_series_annotation_datasets.csv"
+features_cfg_path = "./config/time_series_annotation_datasets_fields.csv"
+processed_datasets_path = "./datasets/processed/"
