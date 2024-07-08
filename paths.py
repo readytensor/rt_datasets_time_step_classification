6a5d96a2d803c2a5b08476c4d5757a5d0500af50
rt-desai <abhyuday.desai@readytensor.com>
2024-07-08 16:15:33 -0700

    update name to time step classification

--- paths.py
+++ paths.py
@@ -1,3 +1,3 @@
-dataset_cfg_path = "./config/time_series_annotation_datasets.csv"
-features_cfg_path = "./config/time_series_annotation_datasets_fields.csv"
+dataset_cfg_path = "./config/time_step_classification_datasets.csv"
+features_cfg_path = "./config/time_step_classification_datasets_fields.csv"
 processed_datasets_path = "./datasets/processed/"
